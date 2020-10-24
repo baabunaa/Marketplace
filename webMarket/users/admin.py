d7477f78a779765b6788bf60f3d57eec6714e65d
baabunaa <babunashvilibeka@gmail.com>
2020-10-24 02:04:12 +0200

    Registration, User and Post classes are created in models

--- webMarket/users/admin.py
+++ webMarket/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # from .models import users
-from .models import User
+from .models import User, RegistrationDetail, Post
 # Register your models here.
 admin.site.register(User)
+admin.site.register(RegistrationDetail)
+admin.site.register(Post)
