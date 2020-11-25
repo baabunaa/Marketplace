897f4557bf125b72c776983279d413b54cd01888
baabunaa <babunashvilibeka@gmail.com>
2020-11-26 00:42:28 +0100

    User can create a post

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
+from users.models import UserAccount
 from .forms import PostForm
 # Create your views here.
 def home_view(request):
@@ -20,30 +21,54 @@ def home_view(request):
 
 
 def sports_view(request):
-    sport_products = Post.objects.filter(category='sp')
-    context = {'title': 'Sports', 'all_products': sport_products}
+    last_three = Post.objects.filter(category='sp')[:3]
+    sport_products = Post.objects.filter(category='sp')[3:]
+    context = {'title': 'Sports', 'all_products': sport_products, 'last_three': last_three}
     return render(request, "category_products.html", context)
 
 
 def music_and_art_view(request):
-    ma_products = Post.objects.filter(category='ma')
-    context = {'title': 'Music & Arts', 'all_products': ma_products}
+    last_three = Post.objects.filter(category='ma')[:3]
+    ma_products = Post.objects.filter(category='ma')[3:]
+    context = {'title': 'Music & Arts', 'all_products': ma_products, 'last_three': last_three}
     return render(request, "category_products.html", context)
 
 
 def electronics_view(request):
-    elec_products = Post.objects.filter(category='el')
-    context = {'title': 'Electronics', 'all_products': elec_products}
+    last_three = Post.objects.filter(category='el')[:3]
+    elec_products = Post.objects.filter(category='el')[3:]
+    context = {'title': 'Electronics', 'all_products': elec_products, 'last_three': last_three}
     return render(request, "category_products.html", context)
 
 
 def home_technics_view(request):
-    home_products = Post.objects.filter(category='ht')
-    context = {'title': 'Home Technichs', 'all_products': home_products}
+    last_three = Post.objects.filter(category='ht')[:3]
+    home_products = Post.objects.filter(category='ht')[3:]
+    context = {'title': 'Home Technichs', 'all_products': home_products, 'last_three': last_three}
     return render(request, "category_products.html", context)
 
 
 def post_view(request):
+    redirect_to = "/"
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        category = request.POST.get("category")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+        user = UserAccount.objects.get(user_id=request.user.id)
+        if  form.is_valid():
+            post = Post()
+            post.name = name
+            post.price = price
+            post.category = category
+            post.description = description
+            post.picture = image
+            post.user = user
+            post.save()
+            return HttpResponseRedirect(redirect_to)
     form = PostForm()
     context={'form':form}
     return render(request,"create_post.html",context)
+    
